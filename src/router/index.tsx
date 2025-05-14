c8eb6bee904d29c536ba43a77de9f993e85dcb84
AllanYen <iamallan0917@gmail.com>
2025-05-14 16:59:15 +0800

    20250514 1700

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,22 +1,25 @@
-import {createHashRouter} from 'react-router'
+import { createHashRouter } from 'react-router'
+import { RouteObject } from "react-router-dom";
 import FrontLayout from '../layouts/FrontLayout'
-import {HomePage,ProductsPage,ProductsPageFromWishList,
-    CartPage,CheckoutFormPage,NotFoundPage,ProductDetailPage,
-    CheckoutPaymentPageFromOrders,OrderListsPage} from '../pages/front'
-const routes = [
+import {
+    HomePage, ProductsPage, ProductsPageFromWishList,
+    CartPage, CheckoutFormPage, NotFoundPage, ProductDetailPage,
+    CheckoutPaymentPageFromOrders, OrderListsPage
+} from '../pages/front'
+const routes: RouteObject[] = [
     {
         path: "/",
-        element:<FrontLayout />,
+        element: <FrontLayout />,
         children: [
-            { index:true, element: <HomePage /> },
-            { path:'products',element:<ProductsPage />},
-            { path: "product/:id",element: <ProductDetailPage />,},
+            { index: true, element: <HomePage /> },
+            { path: 'products', element: <ProductsPage /> },
+            { path: "product/:id", element: <ProductDetailPage />, },
             { path: "cart", element: <CartPage /> },
             { path: "checkout", element: <CheckoutFormPage /> },
             { path: "orderList", element: <OrderListsPage /> },
             { path: "payment/:id", element: <CheckoutPaymentPageFromOrders /> },
             { path: "wishList", element: <ProductsPageFromWishList /> },
-            { path: "*",element: <NotFoundPage />,},
+            { path: "*", element: <NotFoundPage />, },
         ]
     }
 ];
